fb8e19c35271b3bef5bc734922ea61745e2e484e
Dakota Clark <dakotaclark92@yahoo.com>
2019-09-17 01:33:30 +0000

    Automatically backed up by Learn

--- db/migrate/001_student_names.rb
+++ db/migrate/001_student_names.rb
@@ -4,14 +4,6 @@
       create_table :posts do |t|
         t.string :first_name
         t.strung :last_name
-  
-    class CreatePosts < ActiveRecord::Migration
-      def change
-        create_table :posts do |t|
-          t.string :title
-          t.text :description
-     
-          t.timestamps null: false
-        end
       end
     end
+  end
